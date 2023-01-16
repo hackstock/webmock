11fa2f368c29df5caf6fb856a9c551550049f1c5
Edward Pie <hackstockpie@gmail.com>
2023-01-16 21:54:24 +0100

    adds tests to validate routing

---
+++ pkg/api/router_test.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/hackstock/webmock/pkg/parsing"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+)
+
+func Test_Router(t *testing.T) {
+	endpoints, err := parsing.ParseEndpoints("./testdata/endpoints.json")
+	assert.NoError(t, err)
+
+	logger := zap.NewExample()
+
+	router := InitRoutes(endpoints, logger)
+
+	for _, endpoint := range endpoints {
+		w := httptest.NewRecorder()
+		req, err := http.NewRequest(endpoint.HTTPMethod, endpoint.URL, nil)
+		assert.NoError(t, err)
+
+		router.ServeHTTP(w, req)
+
+		assert.Equal(t, endpoint.StatusCode, w.Code)
+
+		resBody, err := json.Marshal(endpoint.Response)
+		assert.NoError(t, err)
+		assert.Equal(t, resBody, w.Body.Bytes())
+	}
+}
